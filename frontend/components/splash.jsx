37388a8a996bb0018852eda98ab13193288115c2
Matt Kerchner <manafount@gmail.com>
2017-01-10 18:02:55 -0800

    preliminary styling for splash page

--- frontend/components/splash.jsx
+++ frontend/components/splash.jsx
@@ -7,29 +7,41 @@ class Splash extends React.Component {
 
   render () {
     return (
-      <div>
-        <nav className="group splash-page">
-          <div className="container">
-            <div className="logo">
+      <div className="splash-container">
+        <div className="row">
+          <div className="col s12">
+            <div className="card darken-1">
+              <div className="center-align">
+                <button onClick={this.sendToLogin}
+                        className="waves-effect waves-light btn-large demo-btn">
+                  Demo Login
+                </button>
+              </div>
+              
+              <main>
+                <div className="card-content">
+                  <label>Email Address</label>
+                  <input
+                    type="text"
+                    name="user[email]"
+                    id="email"
+                    onChange={this.updateEmail}/>
+                  <input
+                    type="password"
+                    name="user[password]"
+                    id="password"
+                    onChange={this.updatePassword}/>
+                  <div className="right-align">
+                    <button onClick={this.sendToRegistration}
+                      className="waves-effect waves-light btn-large center-align demo-btn">
+                      Sign In
+                    </button>
+                  </div>
+                </div>
+              </main>
             </div>
-            <button onClick={this.sendToLogin}>Log In</button>
           </div>
-        </nav>
-        <main>
-          <section className="leader">
-            <h1>Manasana</h1>
-            <h2>This is the subtitle!</h2>
-            <input
-              type="text"
-              name="user[email]"
-              id="email"
-              onChange={this.updateEmail}
-              placeholder="name@company.com" />
-              <button onClick={this.sendToRegistration}>
-                Get Started
-              </button>
-          </section>
-        </main>
+        </div>
       </div>
     );
   }
